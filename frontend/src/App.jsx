1c67a19903afbb297cc01a2f173cf515752e3fa4
Anish <wadkaranish44@gmail.com>
2025-06-27 20:03:43 +0530

    Added Sidebar and Message components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
   return <div className="p-4 h-screen flex  items-center justify-center">
       {/* <Login/> */}
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Home/>
   </div>;
 }
 
